45f593a04778af190354ba14c08d0367e88fa60d
Austin Baltes <austinba@gmail.com>
2016-09-22 16:54:13 -0700

    updated port to 80

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ app.get('/boy-names', function(req, res) {
 });
 
 
-app.listen(3000, function() {
-  console.log('app listening on port 3000');
+app.listen(80, function() {
+  console.log('app listening on port 80');
 });
